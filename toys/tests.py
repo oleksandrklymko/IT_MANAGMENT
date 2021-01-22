e992bd7211fa7d781813483e36040fe5ab5937d1
oleksandrklymko <oleksandrklymko@gmail.com>
2021-01-23 00:10:09 +0200

    some commit

--- toys/tests.py
+++ toys/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Toy
+
+
+class MyTestCase(TestCase):
+    def setUp(self):
+        self.toy = Toy.objects.create(title='Lego', price=200, description='For kids 3+', picture='../media/images/gam1.jpeg')
+
+    def test_toys_list(self):
+        client = Client()
+        response = client.get(reverse('index'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'toys/toys.html')
+
+    def test_get_existing_toy(self):
+        self.assertEqual(self.toy.pk, 1)
+
+    def test_get_toy_that_dont_exist(self):
+        self.assertEqual(self.toy.pk, 2)
